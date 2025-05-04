d660eb6484e1446abc45958515b7df2137a9b669
Juan Herrera <38595445+totolino545@users.noreply.github.com>
2025-05-04 17:54:49 -0300

    Update servicios.py

--- servicios.py
+++ servicios.py
@@ -5,13 +5,13 @@ import requests
 class Servicios:
     
     def todos_los_tags():
-            url = 'http://152.53.85.3/json/tags/?hidebroken=true&reverse=true&order=stationcount'
+            url = f'http://152.53.85.3/json/tags/?hidebroken=true&reverse=true&order=stationcount'
             response = requests.get(url)
             data = response.json()
             return data
     
     def buscar_estaciones(name):
-            url = 'http://152.53.85.3/json/stations/search?name={name}&hidebroken=true&limit={limit}&&name=clickcount&reverse=true'
+            url = f'http://152.53.85.3/json/stations/search?name={name}&hidebroken=true&limit={limit}&&name=clickcount&reverse=true'
             response = requests.get(url)
             data = response.json()
             return data
