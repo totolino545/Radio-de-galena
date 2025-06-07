b3ce86bc6d92d25f3ff5969c113aa3157889c709
Juan Herrera <38595445+totolino545@users.noreply.github.com>
2025-06-07 09:03:56 -0300

    Update traductor.py

--- traductor.py
+++ traductor.py
@@ -14,20 +14,4 @@ class Traducir:
             
         except Exception as e:
             print(f"Error en traducción: {str(e)}")
-            return {'error': 'Error interno en la traducción'}, 500from googletrans import Translator
-
-class Traducir:
-    def __init__(self):
-        self.translator = Translator()
-
-    def traducir_texto(self, texto, idioma_destino='es'):
-        try:
-            if not texto:  # Ahora recibe correctamente el parámetro
-                return {'error': 'Texto vacío recibido'}, 400
-                
-            traduccion = self.translator.translate(texto, dest=idioma_destino)
-            return {'texto_traducido': traduccion.text}, 200
-            
-        except Exception as e:
-            print(f"Error en traducción: {str(e)}")
             return {'error': 'Error interno en la traducción'}, 500
