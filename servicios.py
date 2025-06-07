28fce0614e910369457bef875fa1d39b89618b81
Juan Herrera <38595445+totolino545@users.noreply.github.com>
2025-06-07 08:42:51 -0300

    Update servicios.py

--- servicios.py
+++ servicios.py
@@ -5,19 +5,19 @@ import requests
 class Servicios:
     
     def todos_los_tags():
-        url = f'http://152.53.85.3/json/tags/?hidebroken=true&reverse=true&order=stationcount'
+        url = f'https://fi1.api.radio-browser.info/json/tags/?hidebroken=true&reverse=true&order=stationcount'
         response = requests.get(url)
         data = response.json()
         return data
     
     def buscar_estaciones(name):
-        url = f'http://152.53.85.3/json/stations/search?name={name}&hidebroken=true&limit={limit}&&name=clickcount&reverse=true'
+        url = f'https://fi1.api.radio-browser.info/json/stations/search?name={name}&hidebroken=true&limit={limit}&&name=clickcount&reverse=true'
         response = requests.get(url)
         data = response.json()
         return data
     
     def cargar_estaciones(name, limit=10):
-        url = f'http://152.53.85.3/json/stations/search?name={name}&hidebroken=true&&limit={limit}&reverse=true&order=clickcount'
+        url = f'https://fi1.api.radio-browser.info/json/stations/search?name={name}&hidebroken=true&&limit={limit}&reverse=true&order=clickcount'
         response = requests.get(url)
         data = response.json()
         total = len(data)  # Ajusta esto según tu API
